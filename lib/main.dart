689dbc5fc09f745767a44476c92d3dc115312822
Vu Pham <cr.vanvu@gmail.com>
2022-05-21 04:58:28 +0700

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -53,9 +53,36 @@ class MyApp extends StatelessWidget {
           const Locale('en', ''),
           const Locale('vi', ''),
         ],
+        theme: ThemeData(
+          buttonTheme: Theme.of(context).buttonTheme.copyWith(
+                highlightColor: Color.fromARGB(255, 35, 111, 87),
+              ),
+          primarySwatch: createMaterialColor(Color.fromARGB(255, 35, 111, 87)),
+        ),
         home: HomePage(),
       ),
     );
+  }
+
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    ;
+    return MaterialColor(color.value, swatch);
   }
 }
 
@@ -424,8 +451,8 @@ class ApplicationState extends ChangeNotifier {
   }
 
   Account? getAccount(String accountID) {
-    for(Account acc in _accounts){
-      if (acc.id == accountID){
+    for (Account acc in _accounts) {
+      if (acc.id == accountID) {
         return acc;
       }
     }
